14381994b2f774daa1555f2cbee066b67c2129de
transo <strinhanh@gmail.com>
2023-05-03 16:09:47 +0700

    Update SearchFragment.java

--- Cloud/app/src/main/java/com/example/cloud/fragment/SearchFragment.java
+++ Cloud/app/src/main/java/com/example/cloud/fragment/SearchFragment.java
@@ -1,6 +1,9 @@
 package com.example.cloud.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cloud.R;
+import com.example.cloud.activity.MainActivity;
 import com.example.cloud.activity.RegisterActivity;
 import com.example.cloud.adapter.TepAdapter;
 import com.example.cloud.api.ApiCollection;
@@ -24,9 +28,15 @@ import com.example.cloud.databinding.SearchActivityBinding;
 import com.example.cloud.model.Tep;
 import com.example.cloud.onclick.IOnClickItem;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -102,7 +112,7 @@ public class SearchFragment extends Fragment {
                 tepAdapter = new TepAdapter(SearchFragment.this.getContext(), listTep, R.layout.file_folder, new IOnClickItem() {
                     @Override
                     public void onClickItem(Tep tep) {
-                        taiFile(tep);
+                        taiTep(tep);
                     }
                 });
                 recyclerView.setAdapter(tepAdapter);
@@ -123,7 +133,106 @@ public class SearchFragment extends Fragment {
         }
     }
 
-    private void taiFile(Tep tep) {
+    void taiTep(Tep tep) {
+        MainActivity.tep=tep;
+
+        if(tep.getLoai().equals("video")){
+            replaceFragmentOverlay(new VideoShowFragment());
+        }
+        else if(tep.getLoai().equals("image")){
+            replaceFragmentOverlay(new ImageShowFragment());
+        }
+        else{
+            taiFile(tep);
+        }
+    }
+
+    void taiFolder(Tep tep) {
+        MainActivity.folderPath+=tep.getDuongDan();
+        initRecycleView();
+    }
+
+    private void replaceFragmentOverlay(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_1, fragment);
+        fragmentTransaction.commit();
+    }
+
+    void taiFile(Tep tep) {
+        ApiCollection api = ApiSumoner.callApi();
+
+        Call<ResponseBody> call = api.downloadOneFile(tep.getDuongDan());
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    // Lấy đường dẫn thư mục trên thiết bị để lưu file tải về
+                    // Lưu file vào thư mục tải về.
+                    String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+
+                    File file = new File(directory + "/hdv", tep.getTen());
+                    InputStream inputStream = null;
+                    OutputStream outputStream = null;
+                    try {
+                        byte[] fileReader = new byte[4096];
+                        inputStream = response.body().byteStream();
+                        outputStream = new FileOutputStream(file);
+                        while (true) {
+                            int read = inputStream.read(fileReader);
+                            if (read == -1) {
+                                break;
+                            }
+                            outputStream.write(fileReader, 0, read);
+                        }
+                        outputStream.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (inputStream != null) {
+                            try {
+                                inputStream.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (outputStream != null) {
+                            try {
+                                outputStream.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    Log.e("ss", "2");
+
+                    moFile(file);
+
+
+                } else {
+                    // Xử lý tải file không thành công.
+                    Log.e("fall1", "3");
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                // Xử lý lỗi tải file.
+                Log.e("fall", t.getMessage());
+            }
+        });
+
+    }
+
+    private void moFile(File file) {
+        Uri uri = Uri.fromFile(file);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String mime = getActivity().getContentResolver().getType(uri);
+        intent.setDataAndType(uri, mime);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
     }
 
 
